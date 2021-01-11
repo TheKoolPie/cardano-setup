67ba409aeb2cf9c4eac5b873f04bcc74d8a2fb55
KoolPie <philipp.noecker@gmx.de>
2021-01-11 17:59:28 +0100

    cabal ghc

---
+++ scripts/install-cabal.sh
@@ -0,0 +1,7 @@
+#Install Cabal
+cd "$HOME"
+wget https://downloads.haskell.org/~cabal/cabal-install-3.2.0.0/cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
+tar -xf cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz
+rm cabal-install-3.2.0.0-x86_64-unknown-linux.tar.xz cabal.sig
+mkdir -p "$HOME/.local/bin"
+mv cabal "$HOME/.local/bin/"
