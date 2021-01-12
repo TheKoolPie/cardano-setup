d2a590b1483aae9d256131ab3deb1351b98ad772
KoolPie <philipp.noecker@gmx.de>
2021-01-12 22:54:48 +0100

    add register pool

--- scripts/maintenance/get-file-from-server.ps1
+++ scripts/maintenance/get-file-from-server.ps1
@@ -1,4 +1,11 @@
-$SERVER_FILE_PATH = Read-Host "Please enter path of remote file"
-$LOCAL_FILE_PATH = Read-Host "Please enter local target path"
+param (
+    [Parameter(Mandatory=$true)]
+    [string]
+    $SERVER_FILE_PATH,
+    [Parameter(Mandatory=$true)]
+    [string]
+    $LOCAL_FILE_PATH
+)
 
-scp cardano@core1.ada-cadabra.com:$SERVER_FILE_PATH $LOCAL_FILE_PATH
+
+scp cardano@core1.ada-cadabra.com:"/home/cardano/$SERVER_FILE_PATH" $LOCAL_FILE_PATH
